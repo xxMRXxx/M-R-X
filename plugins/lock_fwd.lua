85e60a620281c80e69585f93d9dc1a731fac9e63
devlopers1 <hmody19960000@gmail.com>
2016-09-23 07:17:08 +0300

    Add files via upload

---
+++ plugins/lock_fwd.lua
@@ -0,0 +1,48 @@
+--[[ 
+# 
+#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ 
+#:(( 
+# For More Information ....! 
+# Developer : Aziz < @TH3_GHOST > 
+# our channel: @DevPointTeam 
+# Version: 1.1 
+#:)) 
+#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ 
+# 
+]] 
+do 
+
+local function pre_process(msg) 
+    --Checking mute 
+    local hash = 'mate:'..msg.to.id 
+    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then 
+            delete_msg(msg.id, ok_cb, true) 
+            send_large_msg(get_receiver(msg), 'عزيزي '..msg.from.first_name..'\nممنوع عمل اعادة توجيه من القنوات هنا التزم بقوانين المجموعة 👋👮\n#username @'..msg.from.username) 
+            return "done" 
+        end 
+        return msg 
+    end 
+
+local function DevPoint(msg, matches) 
+    chat_id =  msg.to.id 
+    if is_momod(msg) and matches[1] == 'قفل' then 
+                    local hash = 'mate:'..msg.to.id 
+                    redis:set(hash, true) 
+                    return "" 
+  elseif is_momod(msg) and matches[1] == 'فتح' then 
+                    local hash = 'mate:'..msg.to.id 
+                    redis:del(hash) 
+                    return "" 
+end 
+
+end 
+
+return { 
+    patterns = { 
+        '^(قفل) اعادة توجيه$', 
+        '^(فتح) اعادة توجيه$' 
+    }, 
+    run = DevPoint, 
+    pre_process = pre_process 
+} 
+end 
--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -1,68 +1,53 @@
---[[
-#
-#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#:((
-# For More Information ....! 
-# Developer : Aziz < @TH3_GHOST > 
-# our channel: @DevPointTeam
-# Version: 1.1
-#:))
-#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#
-]]
-local function DevPoint(msg, matches, callback, extra)
-
-local data = load_data(_config.moderation.data)
-
-local group_welcome = data[tostring(msg.to.id)]['group_welcome']
--------------------------- Data Will be save on Moderetion.json @DevPointCH
+do
     
-if matches[1] == 'delwlc' and not matches[2] and is_owner(msg) then 
-    
-   data[tostring(msg.to.id)]['group_welcome'] = nil --delete welcome
-        save_data(_config.moderation.data, data)
-        
-        return 'Group welcome Deleted!'
-end
-if not is_owner(msg) then 
-    return 'For Owners Only!'
-end
---------------------Loading Group Rules
-local rules = data[tostring(msg.to.id)]['rules']
-    
-if matches[1] == 'rules' and matches[2] and is_owner(msg) then
-    if data[tostring(msg.to.id)]['rules'] == nil then --when no rules found....
-        return 'No Rules Found!\n\nSet Rule first by /set rules [rules]\nOr\nset normal welcome by /setwlc [wlc msg]'
-end
-data[tostring(msg.to.id)]['group_welcome'] = matches[2]..'\n\nGroup Rules :\n'..rules
-        save_data(_config.moderation.data, data)
-        
-        return 'Group welcome Seted To :\n'..matches[2]
-end
-if not is_owner(msg) then 
-    return 'For Owners Only!'
-end--DevPointCH
-
-if matches[1] and is_owner(msg) then
-    
-data[tostring(msg.to.id)]['group_welcome'] = matches[1]
-        save_data(_config.moderation.data, data)
-        
-        return 'Group welcome Seted To : \n'..matches[1]
-end
-if not is_owner(msg) then 
-    return 'For Owners Only!'
-end--@DevPointCH
-
-
-    
+local function axis(msg,matches)
+    if matches[1] == "chat_add_user"  then 
+     local text = 'اهلا 😊👋 بك عزيزي في المجموعة 👥🔕'..'\n'..'\n'
+     ..'المعلومات 📋 الخاصة بك 🔷'..'\n'
+    ..'📌 اسمك :  '..msg.action.user.print_name..'\n'
+    ..'📌 معرفك : @'..(msg.action.user.username or "لا يوجد")..'\n'
+    ..'💭 الايدي : '..msg.action.user.id..'\n'
+    ..'📱رقم الهاتف : '..(msg.action.user.phone or "لا يوجد")..'\n'
+    ..'➖➖➖➖➖ـ'..'\n'
+    ..'📌 اسم المجموعة : '..msg.to.title..'\n'
+    ..'💭 ايدي المجموعة : '..msg.to.id..'\n'
+    ..'➖➖➖➖➖ـ'..'\n'
+    ..'✝ ضافك : '..msg.from.print_name..'\n'
+    ..'✝ معرفة : @'..(msg.from.username or "لا يوجد")..'\n'
+..' ايدية 🆔 : '..msg.from.id..'\n'
+    ..'📱 رقم هاتفةة : '..(msg.from.phone or "لا يوجد")..'\n'
+    ..'➖➖➖➖➖ـ'..'\n'
+    ..'📅 التاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp)
+    ..'🕚 الوقت : '..os.date(' %T*', os.time())..'\n'  
+     ..'➖➖➖➖➖ـ'..'\n'
+    ..'🕵 مطور البوت : @wounds1'..'\n'
+    ..'🤖 معرف البوت : @wounds1_bot'..'\n'..'\n'
+     return reply_msg(msg.id, text, ok_cb, false)
+     end
+    if matches[1] == "chat_add_user_link" then
+        local text = 'اهلا 😊👋 بك عزيزي في المجموعة 👥🔕'..'\n'..'\n'
+     ..'المعلومات 📋 الخاصة بك 🔷'..'\n'
+    ..'📌 اسمك :  '..msg.from.print_name..'\n'
+    ..'📌 معرفك : @'..(msg.from.username or "لا يوجد")..'\n'
+    ..'💭 الايدي : '..msg.from.id..'\n'
+    ..'📱رقم الهاتف : '..(msg.from.phone or "لا يوجد")..'\n'
+    ..'➖➖➖➖➖ـ'..'\n'
+    ..'📌 اسم المجموعة : '..msg.to.title..'\n'
+    ..'💭 ايدي المجموعة : '..msg.to.id..'\n'
+    ..'➖➖➖➖➖ـ'..'\n'
+    ..'📅 التاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp)
+    ..'🕚 الوقت : '..os.date(' %T*', os.time())..'\n'  
+     ..'➖➖➖➖➖ـ'..'\n'
+    ..'🕵 مطور البوت : @wounds1'..'\n'
+    ..'🤖 معرف البوت : @wounds1_bot'..'\n'..'\n'
+        return reply_msg(msg.id, text, ok_cb, false)
+  end
 end
 return {
-  patterns = {
-  "^[!#/]setwlc (rules) +(.*)$",
-  "^[!#/]setwlc +(.*)$",
-  "^[!#/](delwlc)$"
-  },
-  run = DevPoint
+    patterns = {
+        "^!!tgservice (chat_add_user)$",
+        "^!!tgservice (chat_add_user_link)$",
+    },
+ run = axis
 }
---post by channel @DevPointCH
+end
